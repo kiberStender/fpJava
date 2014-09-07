2eb3f2291869c9bd18dbd5d106b1d4efda736543
Kleber Stender <kleber.stender@gmail.com>
2014-09-07 19:32:10 -0300

    Created Nothing singleton

---
+++ src/main/java/br/fpJava/maybe/Nothing.java
@@ -0,0 +1,18 @@
+package br.fpJava.maybe;
+
+/**
+ * Created by sirkleber on 07/09/14.
+ */
+
+public class Nothing extends Maybe<Object>{
+    private static Nothing ourInstance = null;
+
+    public static Nothing none() {
+        if(ourInstance == null){
+            ourInstance = new Nothing();
+        }
+        return ourInstance;
+    }
+
+    private Nothing() {}
+}
