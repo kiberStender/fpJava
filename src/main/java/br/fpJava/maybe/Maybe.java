297d3127973d466d5df8e5b027473f37fb0e4095
Kleber Stender <kleber.stender@gmail.com>
2014-09-07 19:38:21 -0300

    Monad Maybe created

---
+++ src/main/java/br/fpJava/maybe/Maybe.java
@@ -0,0 +1,30 @@
+package br.fpJava.maybe;
+
+import br.fpJava.fn.Fn1;
+import br.fpJava.typeclasses.Monad;
+import static br.fpJava.maybe.Nothing.none;
+
+/**
+ * Created by sirkleber on 07/09/14.
+ */
+
+public abstract class Maybe<A> implements Monad<Maybe, A>{
+
+    public <B> Maybe<B> map(Fn1<A, B> f){
+        if(this instanceof Nothing){
+            return (Maybe<B>) none();
+        } else {
+            Just<A> j = (Just<A>) this;
+            return new Just<>(f.apply(j.a));
+        }
+    }
+
+    public <B> Maybe<B> flatMap(Fn1<A, Monad<Maybe, B>> f){
+        if(this instanceof Nothing){
+            return (Maybe<B>) none();
+        } else {
+            Just<A> j = (Just<A>) this;
+            return (Just<B>) f.apply(j.a);
+        }
+    }
+}
