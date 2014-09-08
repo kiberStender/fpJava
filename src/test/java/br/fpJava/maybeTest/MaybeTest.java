1291863f58a3340a2be7524554142c81c1cf47e0
Kleber Stender <kleber.stender@gmail.com>
2014-09-07 21:31:05 -0300

    Created Maybe test suite

---
+++ src/test/java/br/fpJava/maybeTest/MaybeTest.java
@@ -0,0 +1,82 @@
+package br.fpJava.maybeTest;
+
+import br.fpJava.fn.Fn;
+import br.fpJava.fn.Fn1;
+import br.fpJava.maybe.Just;
+import br.fpJava.maybe.Maybe;
+import static br.fpJava.maybe.Nothing.none;
+
+import br.fpJava.typeclasses.Monad;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by sirkleber on 07/09/14.
+ */
+
+public class MaybeTest {
+
+    private final Fn1<Double, Fn1<Double, Maybe<Double>>> div = new Fn1<Double, Fn1<Double, Maybe<Double>>>() {
+        @Override
+        public Fn1<Double, Maybe<Double>> apply(final Double a) {
+            return new Fn1<Double, Maybe<Double>>() {
+                @Override
+                public Maybe<Double> apply(final Double b) {
+                    if(b == 0) {
+                        return (Maybe<Double>) none();
+                    } else {
+                        return new Just<>(a / b);
+                    }
+                }
+            };
+        }
+    };
+
+    @Test
+    public void testMap() throws NoSuchElementException{
+
+        Maybe<Double> mdb = div.apply(2.0).apply(2.0).map(new Fn1<Double, Double>() {
+            @Override
+            public Double apply(Double aDouble) {
+                return aDouble * 3;
+            }
+        });
+
+        assertTrue(mdb.get() == 3.0);
+    }
+
+    @Test
+    public void testFlatMap() throws NoSuchElementException{
+        Maybe<Double> mbd = div.apply(4.0).apply(2.0).flatMap(new Fn1<Double, Monad<Maybe, Double>>() {
+            @Override
+            public Monad<Maybe, Double> apply(Double aDouble) {
+                return new Just<Double>(aDouble * 2.0);
+            }
+        });
+
+        assertTrue(mbd.get() == 4.0);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGet(){
+        Maybe<Double> mdb = div.apply(4.0).apply(0.0);
+
+        assertTrue(mdb.get() > 0);
+    }
+
+    @Test
+    public void testGetOrElse(){
+        Maybe<Double> mdb = div.apply(4.0).apply(2.0);
+
+        assertTrue(((Double) mdb.getOrElse(new Fn<Object>() {
+            @Override
+            public Object apply() {
+                return "Error";
+            }
+        })) == 2.0);
+    }
+
+}
