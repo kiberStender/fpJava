818e345d8866c24e83bb0049f9bcc93f219d0a69
Kleber Stender <kleber.stender@gmail.com>
2014-09-09 09:39:51 -0300

    Created Nil representing emptiness in a Seq

---
+++ src/main/java/br/fpJava/collections/seq/Nil.java
@@ -0,0 +1,22 @@
+package br.fpJava.collections.seq;
+
+/**
+ * Created by sirkleber on 09/09/14.
+ */
+public class Nil extends Seq<Object>{
+    private static Nil ourInstance = null;
+
+    public static Seq<?
+
+
+
+            > nil() {
+        if(ourInstance == null){
+            ourInstance = new Nil();
+        }
+        return ourInstance;
+    }
+
+    private Nil() {
+    }
+}
