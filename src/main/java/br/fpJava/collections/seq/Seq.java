6c8d2e84ee76b0a7175b483c95f0c840602c8fb1
Kleber Stender <kleber.stender@gmail.com>
2014-09-09 10:19:52 -0300

    Added some methods to Seq abstract class

--- src/main/java/br/fpJava/collections/seq/Seq.java
+++ src/main/java/br/fpJava/collections/seq/Seq.java
@@ -1,10 +1,123 @@
 package br.fpJava.collections.seq;
 
-import br.fpJava.collections.*;
+import br.fpJava.fn.Fn1;
+import br.fpJava.typeclasses.Monad;
+import static br.fpJava.collections.seq.Nil.nil;
+
+import java.util.Arrays;
 
 /**
  * Created by sirkleber on 09/09/14.
  */
 public abstract class Seq<A> extends br.fpJava.collections.Iterable<Seq, A> {
 
+    public Seq<A> cons(A item){
+        return new Cons<>(item, this);
+    }
+
+    public static <A> Seq<A> seq(final A ...a){
+        if(a.length == 0){
+            return (Seq<A>) nil();
+        } else {
+            java.util.List<A> list = Arrays.asList(a);
+            Seq<A> tmplst = (Seq<A>) nil();
+
+            for(A l : list){
+                tmplst = tmplst.cons(l);
+            }
+
+            return tmplst.reverse();
+        }
+    }
+
+    public Integer lenght(){
+        return foldLeft(0, new Fn1<Integer, Fn1<A, Integer>>() {
+            @Override
+            public Fn1<A, Integer> apply( final Integer acc) {
+                return new Fn1<A, Integer>() {
+                    @Override
+                    public Integer apply(A ig) {
+                        return acc + 1;
+                    }
+                };
+            }
+        });
+    }
+
+    public String toString(){
+        return "Seq(" + foldRight("", new Fn1<A, Fn1<String, String>>() {
+            @Override
+            public Fn1<String, String> apply(final A item) {
+                return new Fn1<String, String>() {
+                    @Override
+                    public String apply(String acc) {
+                        if(acc.equals("")){
+                            return "" + item;
+                        } else {
+                            return acc + ", " + item;
+                        }
+                    }
+                };
+            }
+        }) + ")";
+    }
+
+    public Seq<A> reverse(){
+        return foldLeft(((Seq<A>) seq()), new Fn1<Seq<A>, Fn1<A, Seq<A>>>() {
+            @Override
+            public Fn1<A, Seq<A>> apply(final Seq<A> acc) {
+                return new Fn1<A, Seq<A>>() {
+                    @Override
+                    public Seq<A> apply(A item) {
+                        return acc.cons(item);
+                    }
+                };
+            }
+        });
+    }
+
+    @Override
+    public <B> B foldLeft(final B acc, final Fn1<B, Fn1<A, B>> f) {
+        if(this instanceof Nil){
+            return acc;
+        } else {
+            Cons<A> c = (Cons<A>) this;
+            return c.tail.foldLeft(f.apply(acc).apply(c.head), f);
+        }
+    }
+
+    @Override
+    public <B> B foldRight(final B acc, final Fn1<A, Fn1<B, B>> f) {
+        return reverse().foldLeft(acc, new Fn1<B, Fn1<A, B>>() {
+            @Override
+            public Fn1<A, B> apply(final B accu) {
+                return new Fn1<A, B>() {
+                    @Override
+                    public B apply(A item) {
+                        return f.apply(item).apply(accu);
+                    }
+                };
+            }
+        });
+    }
+
+    @Override
+    public <B> Monad<Seq, B> map(Fn1<A, B> f) {
+        return null;
+    }
+
+    @Override
+    public <B> Seq<B> flatMap(final Fn1<A, Monad<Seq, B>> f) {
+        return foldRight((Seq<B>) seq(), new Fn1<A, Fn1<Seq<B>, Seq<B>>>() {
+            @Override
+            public Fn1<Seq<B>, Seq<B>> apply(final A item) {
+                return new Fn1<Seq<B>, Seq<B>>() {
+                    @Override
+                    public Seq<B> apply(Seq<B> acc) {
+                        return null;//acc.cons((Seq<B>) f.apply(item));
+                    }
+                };
+            }
+        });
+    }
 }
