523b76eea56cd47974147360f3179bc4cc267549
Kleber Stender <kleber.stender@gmail.com>
2014-09-09 09:03:12 -0300

    created seq package and abstract class

---
+++ src/main/java/br/fpJava/collections/seq/Seq.java
@@ -0,0 +1,10 @@
+package br.fpJava.collections.seq;
+
+import br.fpJava.collections.*;
+
+/**
+ * Created by sirkleber on 09/09/14.
+ */
+public abstract class Seq<A> extends br.fpJava.collections.Iterable<Seq, A> {
+
+}
