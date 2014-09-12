81a79fed31add85b348676fa3db87ed8c72e921d
Kleber Stender <kleber.stender@gmail.com>
2014-09-12 07:02:14 -0300

    New tests implemented, old tests removed, and new ones coming to substitute

--- src/test/java/br/fpJava/seqTest/SeqTest.java
+++ src/test/java/br/fpJava/seqTest/SeqTest.java
@@ -1,58 +1,90 @@
 package br.fpJava.seqTest;
 
 import br.fpJava.collections.seq.Seq;
+import br.fpJava.collections.seq.Cons;
 import br.fpJava.fn.Fn1;
 import br.fpJava.typeclasses.Monad;
-import junit.framework.Assert;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertTrue;
 import static br.fpJava.collections.seq.Seq.seq;
+import static br.fpJava.collections.seq.Nil.nil;
 
 /**
  * Created by sirkleber on 09/09/14.
  */
 public class SeqTest {
-    @Test
-    public void testSeqLength(){
-        Seq<Integer> seqi = seq(1, 2, 3);
+    private Seq<Integer> s = seq(1, 2, 3);
+    private Seq<Integer> seqi = new Cons<>(1, new Cons<>(2, new Cons<>(3, (Seq<Integer>) nil())));
+    private Seq<String> seqs = seq("Joao", "Luiz");
 
+    @Test
+    public void tesEquality(){
+        assertTrue(seqi.equals(seqi));
+    }
+
+    @Test
+    public void testEquality1(){
+        assertTrue(s.equals(s));
+    }
+
+    @Test
+    public void testEquality2(){
+        assertTrue(seqi.equals(s));
+    }
+
+    @Test
+    public void testEquality3(){
+        assertTrue(s.equals(seqi));
+    }
+
+    @Test
+    public void testEquality4(){
+        assertTrue(!seqi.equals(seqs));
+    }
+
+    @Test
+    public void testEquality5(){
+        assertTrue(!s.equals(seqs));
+    }
+
+    @Test
+    public void testToString(){
+        assertTrue(seqi.toString().equals("Seq(1, 2, 3)"));
+    }
+
+    @Test
+    public void testToString1(){
+        assertTrue(s.toString().equals("Seq(1, 2, 3)"));
+    }
+
+    @Test
+    public void testToString2(){
+        assertTrue(seqs.toString().equals("Seq(Joao, Luiz)"));
+    }
+
+    @Test
+    public void testLength(){
         assertTrue(seqi.length().equals(3));
     }
 
     @Test
-    public void testSeqCons(){
-        Seq<Integer> seqi = seq(1).cons(2).cons(3).cons(4);
+    public void testLength1(){
+        assertTrue(s.length().equals(3));
+    }
 
-        assertTrue(seqi.length().equals(4));
+    @Test
+    public void testLength2(){
+        assertTrue(seqs.length().equals(2));
+    }
 
+    @Test
+    public void testCons(){
+        assertTrue(seqi.cons(0).equals(seq(0, 1, 2, 3)));
     }
 
     @Test
     public void testConcat(){
-        Seq<Integer> seqi = seq(1, 2, 3, 4);
-        Seq<Integer> seqi2 = seq(5, 6, 7);
-
-        assertTrue(seqi.concat(seqi2).equals(seq(1, 2, 3, 4, 5, 6, 7)));
-    }
-
-    @Test
-    public void testMap(){
-        assertTrue(seq(1, 2, 3).map(new Fn1<Integer, Integer>() {
-            @Override
-            public Integer apply(Integer x) {
-                return x * 2;
-            }
-        }).equals(seq(2, 4, 6)));
-    }
-
-    @Test
-    public void testFlatMap(){
-        assertTrue(seq(1, 2, 3).flatMap(new Fn1<Integer, Monad<Seq, Integer>>() {
-            @Override
-            public Monad<Seq, Integer> apply(Integer x) {
-                return seq(x + 1);
-            }
-        }).equals(seq(2, 3, 4)));
+        assertTrue(seqi.concat(s).equals(seq(1, 2, 3, 1, 2, 3)));
     }
 }
