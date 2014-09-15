1e0fa9e461d9805372a8dfadfcc13c1971f59b69
Kleber Stender <kleber.stender@gmail.com>
2014-09-15 09:32:47 -0300

    Added comment to map function

--- src/main/java/br/fpJava/typeclasses/Functor.java
+++ src/main/java/br/fpJava/typeclasses/Functor.java
@@ -3,5 +3,12 @@ package br.fpJava.typeclasses;
 import br.fpJava.fn.Fn1;
 
 public interface Functor<F, A>{
+    /**
+     * Method to iterate over the container and produce a new container
+     * applying the f anonymous function
+     * @param f Function to transform the items inside the container
+     * @param <B> The type of anonymous function return
+     * @return A new container with type B
+     */
   <B> Functor<F, B> map(Fn1<A, B> f);
 }
