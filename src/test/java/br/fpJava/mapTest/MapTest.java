ba4a538587472cfd21e5b9d14039a7dd6ae06d90
Kleber Stender <kleber.stender@gmail.com>
2014-09-15 14:37:57 -0300

    Added foldLeft and foldRight tests

--- src/test/java/br/fpJava/mapTest/MapTest.java
+++ src/test/java/br/fpJava/mapTest/MapTest.java
@@ -41,4 +41,34 @@ public class MapTest {
         assertTrue(mi.get(2).get().equals("eduardo"));
     }
 
+    @Test
+    public void testFoldLeft(){
+        assertTrue(mi.foldLeft(0, new Fn1<Integer, Fn1<Tuple2<Integer, String>, Integer>>() {
+            @Override
+            public Fn1<Tuple2<Integer, String>, Integer> apply(final Integer acc) {
+                return new Fn1<Tuple2<Integer, String>, Integer>() {
+                    @Override
+                    public Integer apply(final Tuple2<Integer, String> item) {
+                        return acc - item._1;
+                    }
+                };
+            }
+        }).equals(-3));
+    }
+
+    @Test
+    public void testFoldRight(){
+        assertTrue(mi.foldRight(0, new Fn1<Tuple2<Integer, String>, Fn1<Integer, Integer>>() {
+            @Override
+            public Fn1<Integer, Integer> apply(final Tuple2<Integer, String> item) {
+                return new Fn1<Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer acc) {
+                        return acc - item._1;
+                    }
+                };
+            }
+        }).equals(-3));
+    }
+
 }
