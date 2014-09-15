8a8ddb1edd7d7d6ee2467ef7fcff9d1c88be5645
Kleber Stender <kleber.stender@gmail.com>
2014-09-15 08:36:58 -0300

    Created class Map and package map

---
+++ src/main/java/br/fpJava/collections/map/Map.java
@@ -0,0 +1,12 @@
+package br.fpJava.collections.map;
+
+import br.fpJava.collections.Traversable;
+import br.fpJava.tuple.Tuple2;
+
+
+/**
+ * Created by sirkleber on 15/09/14.
+ */
+
+public abstract class Map<K, V> extends Traversable<Map, Tuple2>{
+}
