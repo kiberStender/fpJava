3678094c28fc0ba6ab3f2fdde07ce451bf3c48d1
sirkleber <kleber.stender@gmail.com>
2014-10-10 13:24:49 -0300

    Created Set abstract class

---
+++ src/main/java/br/fpJava/collections/set/Set.java
@@ -0,0 +1,14 @@
+package br.fpJava.collections.set;
+
+import br.fpJava.collections.Traversable;
+import br.fpJava.maybe.Maybe;
+import br.fpJava.tuple.Tuple2;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by sirkleber on 10/10/14.
+ */
+public abstract class Set<A extends Comparable<A>> extends Traversable<Set, A> {
+
+}
